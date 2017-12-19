a7af0f7dd457a346875b83d2e283e3307dee8a4b
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-19 16:35:36 -0500

    Eastgate Square 2.0 1.0  /main.js

--- main.js
+++ main.js
@@ -105,7 +105,7 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
             },
             {
                 name: "Leasing",
-                id: "stores_link",
+                id: "leasing_link",
                 // href: "/stores",
                 parent_id: "stores",
                 parent_class_list: "menu_item",
