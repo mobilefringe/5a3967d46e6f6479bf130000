4f412c02577da4d3fd7f02ccd8f0cd182e2cf219
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-19 14:45:04 -0500

    Eastgate Square 2.0 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -80,7 +80,7 @@
                 feature_items () {
                     console.log(this.$store.state.results.feature_items);
                     return _.slice(this.$store.state.results.feature_items, 2,this.$store.state.results.feature_items.length );
-                    var temp_val = {}
+                    var temp_val = {};
                 }
             },
             methods: {
