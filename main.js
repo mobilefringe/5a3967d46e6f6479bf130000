f1bbff5d27b48771b1d2b8c92035d9228133525f
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-19 16:31:28 -0500

    Eastgate Square 2.0 1.0  /main.js

--- main.js
+++ main.js
@@ -72,11 +72,13 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
                 parent_class_list: "menu_item",
                 hover_id: "stores_underline",
                 hover_class_list:"menu_underline",
-                sub_menu : {
+                sub_menu : [
+                    {
                     name: "Store Directory",
                     id: "stores_link",
                     href: "/stores"
                 }
+                ]
             },
             {
                 name: "Sales & Promos",
