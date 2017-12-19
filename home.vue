157aa2a7dcec0a3cd56e187e05a5cffca67e67b1
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-19 14:46:51 -0500

    Eastgate Square 2.0 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -80,6 +80,7 @@
                 feature_items () {
                     console.log(this.$store.state.results.feature_items);
                     // return _.slice(this.$store.state.results.feature_items, 2,this.$store.state.results.feature_items.length );
+                    new_features = _.slice(this.$store.state.results.feature_items, 2,this.$store.state.results.feature_items.length );
                     var temp_val = {};
                     temp_val.name = "Temp Feature";
                     temp_val.description = "Temp Feature description";
