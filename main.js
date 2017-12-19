5073e8292a4c8d4c79d543a7906b4db6a6c5810a
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-19 16:31:45 -0500

    Eastgate Square 2.0 1.0  /main.js

--- main.js
+++ main.js
@@ -79,7 +79,7 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
                         href: "/stores"
                     },
                     {
-                        name: "Store Directory",
+                        name: "Centre Map",
                         id: "stores_link",
                         href: "/stores"
                     }
