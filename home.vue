22f6477ea98e33254ad2d3909b2e3b28c75683d9
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-19 14:42:03 -0500

    Eastgate Square 2.0 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -20,7 +20,7 @@
             <div class="top_two hidden-phone">
                 <div class="banner_features"  v-for="feature in banner_features">
                     <router-link :to="feature.url">
-                        <img class="top_image" :src='feature.image_url'/>
+                        <img class="top_image" :src='feature.image_url'/><!--<img class="top_image" :src='feature.image_url'/>-->
                         <div class="feature_overlay">{{feature.name}}</div>
                     </router-link>
                 </div>
