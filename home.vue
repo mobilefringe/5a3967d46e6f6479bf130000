74b667db00cfb69bf93251f72c8e517e54e05608
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-19 14:38:13 -0500

    Eastgate Square 2.0 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -72,6 +72,7 @@
                 banners () {
                     console.log(_.orderBy(this.$store.state.results.banners, ['position'], ['asc']));
                     return _.orderBy(this.$store.state.results.banners, ['position'], ['asc']);
+                    // banners: ['http://placehold.it/700x460','http://placehold.it/700x460','http://placehold.it/700x460']
                 },
                 banner_features () {
                    return  _.slice(this.$store.state.results.feature_items, 0, 2);
