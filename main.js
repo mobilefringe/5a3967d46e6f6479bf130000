25dfe3c92f2bd1f5dd4a493def5867473e817d43
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-19 16:34:36 -0500

    Eastgate Square 2.0 1.0  /main.js

--- main.js
+++ main.js
@@ -159,7 +159,7 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
                         href: "/jobs"
                     },
                     {
-                        name: "About Us",
+                        name: "Shopping Hours",
                         id: "stores_link",
                         href: "/map"
                     },
