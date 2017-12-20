0550e931a1d192da33db81a7f6b7982cfb750ee4
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-20 17:31:13 -0500

    Eastgate Square 2.0 1.0  /main.js

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
   });
   
   Vue.component('v-select', VueSelect.VueSelect);
-    Vue.component(Popover)
+    Vue.component(''vue-dropdown',Popover)
   /* bootstrap app */
   const vm = new Vue({
     el: '#app',
