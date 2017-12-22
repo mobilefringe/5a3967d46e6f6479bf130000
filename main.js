02c09c66d5031a1e9833be361618537a6d8d92dd
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-22 14:06:47 -0500

    Eastgate Square 2.0 1.0  /main.js

--- main.js
+++ main.js
@@ -33,6 +33,7 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
     Vue.use(Vue2Filters);
     Vue.use(VueI18n);
     // Vue.use(Popover)
+    Vue.use(BootstrapVue);
   /* initialize router */
   const router = new VueRouter({
     mode: 'history',
