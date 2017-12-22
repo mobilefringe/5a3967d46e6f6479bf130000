266aa6d6e9b76637276b56e6d8b0074e7ac7c492
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-22 14:06:53 -0500

    Eastgate Square 2.0 1.0  /main.js

--- main.js
+++ main.js
@@ -48,7 +48,7 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
   });
   
   Vue.component('v-select', VueSelect.VueSelect);
-  Vue.component('v-select', VueBootstrap);
+//   Vue.component('v-select', VueBootstrap);
   /* bootstrap app */
   const vm = new Vue({
     el: '#app',
