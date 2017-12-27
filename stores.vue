bc3d596f8c6195c6057b96759a39fc9d543f37ae
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-27 09:59:13 -0500

    Eastgate Square 2.0 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -98,7 +98,6 @@
                   return this.$store.getters.processedStores;
                 },
                 allCatergories() {
-                    console.log(this.$store.getters.processedCategories);
                     return this.$store.getters.processedCategories;
                 },
                 dropDownCats () {
