c11e607b6a83ef72ccb2218055b8fccf9cb29192
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-27 17:23:59 -0500

    Eastgate Square 2.0 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -29,38 +29,42 @@
 </template>
 
 <script>
-  define(["Vue"], function(Vue) {
-    return Vue.component("store-details-component", {
-      template: template, // the variable template will be injected,
-      data: function() {
-        return {
-          title: "MM with Vue.js!",
-          description: "An example of integration of Mall Maverick with Vue.js",
-          currentStore: null
-        }
-      },
-      beforeRouteEnter (to, from, next) {
-        next(vm => {
-          // access to component instance via `vm`
-          vm.currentStore = vm.findStoreBySlug(to.params.id);
-          console.log(vm.currentStore );
-          if (vm.currentStore === null || vm.currentStore === undefined){
-            vm.$router.replace({ name: '404'});
-          }
-        })
-      },
-      beforeRouteUpdate (to, from, next) {
-        this.currentStore = this.findStoreBySlug(to.params.id);
-        console.log(this.currentStore );
-        if (this.currentStore === null || this.currentStore === undefined){
-          this.$router.replace({ name: '404'});
-        }
-      },
-      computed: {
-        findStoreBySlug () {
-          return this.$store.getters.findStoreBySlug;
-        }
-      }
+    define(["Vue"], function(Vue) {
+        return Vue.component("store-details-component", {
+            template: template, // the variable template will be injected,
+            data: function() {
+                return {
+                    title: "MM with Vue.js!",
+                    description: "An example of integration of Mall Maverick with Vue.js",
+                    currentStore: null
+                }
+            },
+            beforeRouteEnter(to, from, next) {
+                next(vm => {
+                    // access to component instance via `vm`
+                    vm.currentStore = vm.findStoreBySlug(to.params.id);
+                    console.log(vm.currentStore);
+                    if (vm.currentStore === null || vm.currentStore === undefined) {
+                        vm.$router.replace({
+                            name: '404'
+                        });
+                    }
+                })
+            },
+            beforeRouteUpdate(to, from, next) {
+                this.currentStore = this.findStoreBySlug(to.params.id);
+                console.log(this.currentStore);
+                if (this.currentStore === null || this.currentStore === undefined) {
+                    this.$router.replace({
+                        name: '404'
+                    });
+                }
+            },
+            computed: {
+                findStoreBySlug() {
+                    return this.$store.getters.findStoreBySlug;
+                }
+            }
+        });
     });
-  });
 </script>
