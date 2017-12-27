857d94bfda292593441761661fbfec5860ec8d79
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-27 17:23:41 -0500

    Eastgate Square 2.0 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -3,7 +3,7 @@
     <div class="row" style="margin-left: inherit;">
         <div class="col-md-4">
       <div>
-      <img :src="currentStore.store_front_url_abs"  style="width:300px;height:300px"/>
+        <img :src="currentStore.store_front_url_abs"  style="width:300px;height:300px"/>
       </div>
     </div>
     <div class="col-md-8">
