62fc376e29650eb3f6de70483d868873b376726a
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-27 17:23:45 -0500

    Eastgate Square 2.0 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -2,9 +2,9 @@
   <div class="page_container" v-if="currentStore">
     <div class="row" style="margin-left: inherit;">
         <div class="col-md-4">
-      <div>
-        <img :src="currentStore.store_front_url_abs"  style="width:300px;height:300px"/>
-      </div>
+          <div>
+            <img :src="currentStore.store_front_url_abs"  style="width:300px;height:300px"/>
+          </div>
     </div>
     <div class="col-md-8">
         <img src="http://placehold.it/620x300"/>
