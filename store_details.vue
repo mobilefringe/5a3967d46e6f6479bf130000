938a0d76e0162b78f8b8e0eddbff30fddd99adf4
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-27 12:21:10 -0500

    Eastgate Square 2.0 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -11,7 +11,7 @@
       </div>
     </div>
     <div class="col-md-8">
-    <img src="http://placehold.it/700x460"/>
+    <img src="http://placehold.it/620x300"/>
       <!--<div>-->
       <!--  <p>{{currentStore.description}}</p>-->
       <!--</div>-->
