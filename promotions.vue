a0cd8f397689dafbcd2e67f2e26175c443849132
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-28 16:02:25 -0500

    Eastgate Square 2.0 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -10,7 +10,7 @@
 				</div>
 				<div class="col-sm-5 promo_div_dets">
 					<p class="promo_div_name">{{promo.name}}</p>
-					<p class="promo_div_store_name">{{promotions.store.name | uppercase}}</p>
+					<p class="promo_div_store_name">{{promotions.name | uppercase}}</p>
 					<p class="promo_div_date">{{promo.start_date | moment("MMM D", timezone)}} - {{promo.end_date | moment("MMM D", timezone)}}</p>
 					<p class="promo_div_description">{{promo.description_short}}</p>
 					<span class="feature_read_more">
