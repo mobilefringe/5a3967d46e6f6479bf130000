a5605b71794d44441307ab5cb491c91e629d1849
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-28 15:48:41 -0500

    Eastgate Square 2.0 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -50,11 +50,22 @@
 		<div class="store_promo_container" v-if="currentStore && currentStore.total_published_jobs > 0">
 		    <div class="promo_container_title text-left all_caps"> Sales & Promotions</div>
 		    <div class="row store_promo_dets" v-for="promo in jobs">
-		        <div class="col-sm-7">
+		        <div class="col-sm-7" >
+		        <div class="promo_div_image">
 		            <img :src="promo.image_url" alt=""/>
 		        </div>
-		        <div class="col-sm-5">
+		        </div>
+		        <div class="col-sm-5 promo_div_dets">
+		            <p class="promo_div_name">{{promo.name}}</p>
+		            <p class="promo_div_store_name">{{currentStore.name | uppercase}}</p>
+		            <p class="promo_div_date">{{promo.start_date | moment("MMM D", timezone)}} - {{promo.end_date | moment("MMM D", timezone)}}</p>
 		            
+						<p class="promo_div_description">{{promo.description_short}}</p>
+						<span class="feature_read_more">
+							<router-link :to="'/promotions/'+promo.slug" class="mobile_readmore" >
+								<p class="feature-readmore">Read More <i class="fa fa-chevron-right pull-right" aria-hidden="true"></i></p>
+							</router-link>
+						</span>
 		        </div>
 		    </div>
 		</div>
