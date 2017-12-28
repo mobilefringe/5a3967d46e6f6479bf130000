b514dabd6e3bff734ca669c6aad9a6e361ca3786
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-28 16:20:05 -0500

    Eastgate Square 2.0 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -1,6 +1,7 @@
 <template>
 	<div class="container"><!-- for some reason if you do not put an outer container div this component template will not render -->
 		<div class="date_bar">
+		<span></span>
 		    <i class="fa fa-chevron-left"></i>
 		    <div class="current_date">
 		        {{selectedDate}}
