01278fa20be7456cbbd5511a0394f2e3114499f6
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-28 16:11:00 -0500

    Eastgate Square 2.0 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -1,6 +1,9 @@
 <template>
 	<div class="container"><!-- for some reason if you do not put an outer container div this component template will not render -->
-		<div class="date_bar"></div>
+		<div class="date_bar">
+		    <div></div>
+		    
+		</div>
 		<div class="promo_container" v-if="promotions">
 			<div class="row promo_dets text-left" v-for="promo in promotions">
 				<div class="col-sm-7" >
