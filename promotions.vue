76e92c2395e119b12c53d2a85b50110d78fdaef3
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-28 17:41:55 -0500

    Eastgate Square 2.0 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -77,8 +77,8 @@
                     console.log(this.promotions);
                     this.processedPromos = _.filter(this.promotions, function(val){
                         console.log(val);
-                        start_date = moment(val.start_date).tz(this.timezone).format('MMM D, YYYY');
-                        end_date = moment(val.end_date).tz(this.timezone).format('MMM D, YYYY');
+                        start_date = moment(val.start_date).tz(this.timezone);
+                        end_date = moment(val.end_date).tz(this.timezone);
                         console.log(this.selectedDate, start_date, end_date);
                         // if(this.selectedDate >= start_date && this.selectedDate <= end_date) return true;
                         return this.selectedDate >= start_date && this.selectedDate <= end_dat;
