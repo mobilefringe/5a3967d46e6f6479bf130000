1c77afedec518e63bf90959c7c68fc22ace89164
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-28 16:04:44 -0500

    Eastgate Square 2.0 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -142,7 +142,7 @@
                     _.forEach(this.currentStore.promotions, function(value, key) {
                         // console.log(vm.findPromoById(value));
                         // var current_promo = vm.findPromoById(value);
-                        current_promo.description_short = _.truncate(current_promo.description, {
+                        value.description_short = _.truncate(current_promo.description, {
                             'length': 70
                         });
                         temp_promo.push(current_promo);
