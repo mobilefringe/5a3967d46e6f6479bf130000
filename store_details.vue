dae6bb7f17deb4a144ebdda37381e5c6d8914e97
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-28 15:23:51 -0500

    Eastgate Square 2.0 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -29,7 +29,7 @@
 		    <div class="promo_container_title text-left all_caps"> Sales & Promotions</div>
 		    <div class="row store_promo_dets">
 		        <div class="col-sm-7">
-		            <img :src=""/>
+		            <img :src="" />
 		        </div>
 		        <div class="col-sm-5">
 		            
