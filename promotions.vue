c1d298e24b0475205d5283cf7d3382e94209aee9
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-28 16:58:35 -0500

    Eastgate Square 2.0 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -46,6 +46,13 @@
     .current_date{
         color:#636363;
     }
+    .all_dates span {
+        /* 1 2 3 4 5 6 7 8 9 10: */
+font-family: .AppleSystemUIFont;
+font-size: 16px;
+color: #000000;
+letter-spacing: 1.5px;
+    }
 </style>
 
 <script>
