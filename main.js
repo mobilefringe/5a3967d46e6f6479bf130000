60f0b192d6017c5a1044709ff099465a4e9e0488
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-29 16:20:00 -0500

    Eastgate Square 2.0 1.0  /main.js

--- main.js
+++ main.js
@@ -125,7 +125,7 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
                             {
                                 name: "Speciality Leasing",
                                 id: "stores_link",
-                                href: "/http://eastgatesquare.ca/specialty_leasing"
+                                href: "/specialty_leasing"
                             },
                             {
                                 name: "Photos",
