17b982acb3eb5622b4267f625472f8eb0b6ef63b
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-29 10:25:01 -0500

    Eastgate Square 2.0 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -84,7 +84,7 @@
                         
                         if(selected >= start_date && selected <= end_date) {
                             console.log(selected, start_date, end_date);
-                            console.log(selected.diff(start_date, 'days') >= 0, 0 <= selected.diff(b, 'days'), (selected >= start_date && selected <= end_date));
+                            console.log(selected.diff(start_date, 'days') >= 0, 0 <= selected.diff(end_date, 'days'), (selected >= start_date && selected <= end_date));
                         
                         }
                         return (selected >= start_date && selected <= end_date);
