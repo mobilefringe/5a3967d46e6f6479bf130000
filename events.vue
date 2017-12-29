e8e7dcd5d6c5a2af1f1bf429419d830fb8928915
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-29 12:58:11 -0500

    Eastgate Square 2.0 1.0  /events.vue

--- events.vue
+++ events.vue
@@ -140,7 +140,7 @@
 <script>
     define(["Vue", "moment", "moment-timezone", "vue-moment", "vue-meta"], function(Vue, moment, tz, VueMoment, Meta) {
         Vue.use(Meta);
-        return Vue.component("promos-component", {
+        return Vue.component("events-component", {
             template: template, // the variable template will be injected
             data: function() {
                 return {
