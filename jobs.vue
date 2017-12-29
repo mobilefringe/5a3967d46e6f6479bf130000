f3105bfa81395ccbf0f4c71cf2d53b9f7551475b
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-29 16:43:17 -0500

    Eastgate Square 2.0 1.0  /jobs.vue

--- jobs.vue
+++ jobs.vue
@@ -140,7 +140,7 @@
 <script>
     define(["Vue", "moment", "moment-timezone", "vue-moment", "vue-meta"], function(Vue, moment, tz, VueMoment, Meta) {
         Vue.use(Meta);
-        return Vue.component("events-component", {
+        return Vue.component("jobs-component", {
             template: template, // the variable template will be injected
             data: function() {
                 return {
