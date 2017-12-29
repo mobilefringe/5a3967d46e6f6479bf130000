1ad0a0ff5eace4b770b69592db9c096ba58fe942
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-29 16:21:43 -0500

    Eastgate Square 2.0 1.0  /main.js

--- main.js
+++ main.js
@@ -177,7 +177,7 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
                             {
                                 name: "Community",
                                 id: "stores_link",
-                                href: "/map"
+                                href: "/community"
                             }
                         ],
                     }
