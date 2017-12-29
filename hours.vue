921c885a0c4c6a84673279f98e55df3e18c8b447
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-29 17:26:42 -0500

    Eastgate Square 2.0 1.0  /hours.vue

--- hours.vue
+++ hours.vue
@@ -51,9 +51,6 @@
     .row{
         margin-left:inherit;
     }
-    .hours_container {
-        
-    }
 </style>
 <script>
     define(["Vue", "moment", "moment-timezone", "vue-moment", "vue-meta"], function(Vue, moment, tz, VueMoment, Meta) {
