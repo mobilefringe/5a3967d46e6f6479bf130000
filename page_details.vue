f09ae4b96a70c16cbfbd07e9c51f7fd509482014
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-29 16:18:34 -0500

    Eastgate Square 2.0 1.0  /page_details.vue

---
+++ page_details.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+         <div class="main_container " v-if="currentPage">
+            <h4 class="page_title promo_main_header" v-html="currentPage.title">  </h4>
+            <div class="row padding_30" >
+                <div class="description_text text_left" v-html="currentPage.body">
+                    
+                </div>
+            </div>
+        </div>
+        <div style="padding:20px 0;"></div>
+    </div>
+</template>
+
+<script>
+  define(["Vue", "moment", "moment-timezone", "vue-moment"], function(Vue, moment, tz, VueMoment) {
+    return Vue.component("page-details-component", {
+      template: template, // the variable template will be injected,
+      data: function() {
+        return {
+          success_subscribe : false,
+          currentPage: null
+        }
+      },
+      beforeRouteEnter (to, from, next) {
+        next(vm => {
+          // access to component instance via `vm`
+            vm.$store.dispatch('LOAD_PAGE_DATA', {url:vm.property.mm_host + "/pages/"+ to.params.id +".json"}).then(response => {
+                
+                vm.currentPage = response.data;
+                console.log(vm.currentPage);
+            }, error => {
+                console.error("Could not retrieve data from server. Please check internet connection and try again.");
+                vm.$router.replace({ name: '404'});
+            });
+        })
+      },
+      beforeRouteUpdate (to, from, next) {
+            this.$store.dispatch('LOAD_PAGE_DATA', {url:this.property.mm_host + "/pages/"+ to.params.id +".json"}).then(response => {
+                // this.dataLoaded = true;
+                this.currentPage = response.data;
+                console.log(this.currentPage);
+            }, error => {
+                console.error("Could not retrieve data from server. Please check internet connection and try again.");
+                this.$router.replace({ name: '404'});
+            });
+      },
+      computed: {
+        timezone() {
+          return this.$store.getters.getTimezone;
+        },
+        property (){
+            return this.$store.getters.getProperty;
+        }
+      }
+    });
+  });
+</script>
