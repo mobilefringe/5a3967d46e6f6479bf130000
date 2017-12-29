9be65db8e016de0e635e3f1d9e33171b849f8b2f
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-29 16:25:17 -0500

    Eastgate Square 2.0 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -13,46 +13,54 @@
 </template>
 
 <script>
-  define(["Vue", "moment", "moment-timezone", "vue-moment"], function(Vue, moment, tz, VueMoment) {
-    return Vue.component("page-details-component", {
-      template: template, // the variable template will be injected,
-      data: function() {
-        return {
-          success_subscribe : false,
-          currentPage: null
-        }
-      },
-      beforeRouteEnter (to, from, next) {
-        next(vm => {
-          // access to component instance via `vm`
-            vm.$store.dispatch('LOAD_PAGE_DATA', {url:vm.property.mm_host + "/pages/"+ to.params.id +".json"}).then(response => {
-                
-                vm.currentPage = response.data;
-                console.log(vm.currentPage);
-            }, error => {
-                console.error("Could not retrieve data from server. Please check internet connection and try again.");
-                vm.$router.replace({ name: '404'});
-            });
-        })
-      },
-      beforeRouteUpdate (to, from, next) {
-            this.$store.dispatch('LOAD_PAGE_DATA', {url:this.property.mm_host + "/pages/"+ to.params.id +".json"}).then(response => {
-                // this.dataLoaded = true;
-                this.currentPage = response.data;
-                console.log(this.currentPage);
-            }, error => {
-                console.error("Could not retrieve data from server. Please check internet connection and try again.");
-                this.$router.replace({ name: '404'});
-            });
-      },
-      computed: {
-        timezone() {
-          return this.$store.getters.getTimezone;
-        },
-        property (){
-            return this.$store.getters.getProperty;
-        }
-      }
+    define(["Vue", "moment", "moment-timezone", "vue-moment"], function(Vue, moment, tz, VueMoment) {
+        return Vue.component("page-details-component", {
+            template: template, // the variable template will be injected,
+            data: function() {
+                return {
+                    success_subscribe: false,
+                    currentPage: null
+                }
+            },
+            beforeRouteEnter(to, from, next) {
+                next(vm => {
+                    // access to component instance via `vm`
+                    vm.$store.dispatch('LOAD_PAGE_DATA', {
+                        url: vm.property.mm_host + "/pages/" + to.params.id + ".json"
+                    }).then(response => {
+
+                        vm.currentPage = response.data;
+                        console.log(vm.currentPage);
+                    }, error => {
+                        console.error("Could not retrieve data from server. Please check internet connection and try again.");
+                        vm.$router.replace({
+                            name: '404'
+                        });
+                    });
+                })
+            },
+            beforeRouteUpdate(to, from, next) {
+                this.$store.dispatch('LOAD_PAGE_DATA', {
+                    url: this.property.mm_host + "/pages/" + to.params.id + ".json"
+                }).then(response => {
+                    // this.dataLoaded = true;
+                    this.currentPage = response.data;
+                    console.log(this.currentPage);
+                }, error => {
+                    console.error("Could not retrieve data from server. Please check internet connection and try again.");
+                    this.$router.replace({
+                        name: '404'
+                    });
+                });
+            },
+            computed: {
+                timezone() {
+                    return this.$store.getters.getTimezone;
+                },
+                property() {
+                    return this.$store.getters.getProperty;
+                }
+            }
+        });
     });
-  });
 </script>
