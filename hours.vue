64b36e772c411fc3cefe5c44e210f4559f639431
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-29 17:02:57 -0500

    Eastgate Square 2.0 1.0  /hours.vue

--- hours.vue
+++ hours.vue
@@ -37,7 +37,7 @@
                         
                         <div class="padding_top_20"></div>
                         
-                        <h3 class="hours_heading">Holiday Closures</h3>
+                        <h3 class="hours_heading text-left">Holiday Closures</h3>
                         <div id="closed_hours_container" class="hours_container">
                             <div class="hours_div" v-for="hour in closeHolidays">
                                 <span>{{hour.holiday_name}} ({{hour.holiday_date | moment("MMM D YYYY", timezone)}})</span>
