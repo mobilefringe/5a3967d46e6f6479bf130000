e854777bbd25e49fa02f56f4ebaef4cd3733e757
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-29 12:36:48 -0500

    Eastgate Square 2.0 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -201,7 +201,7 @@
                     month = moment(this.selectedDate).month();
                     year = moment(this.selectedDate).year();
                     this.selectedDate = moment([year, month, val]).format('MMM D, YYYY');
-                    $("date_"+val).addClass('active');
+                    // $("date_"+val).addClass('active');
                 },
                 truncate(val_body){
                     var truncate = _.truncate(val_body, { 'length': 99, 'separator': ' ' });
