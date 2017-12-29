a05d17aca2b4f4ec8156bdcddc16ac7480215e25
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-29 12:58:53 -0500

    Eastgate Square 2.0 1.0  /events.vue

--- events.vue
+++ events.vue
@@ -10,8 +10,8 @@
 		<div class="all_dates text-center">
 		    <span v-for="n in daysInMonth" :id="'date_'+n" @click="newDate(n)">{{n}}</span>
 		</div>
-		<div class="promo_container" v-if="processedPromos.length > 0">
-			<div class="row promo_dets text-left" v-for="promo in processedPromos">
+		<div class="promo_container" v-if="processedEvents.length > 0">
+			<div class="row promo_dets text-left" v-for="promo in processedEvents">
 				<div class="col-sm-7" >
 					<div class="promo_div_image">
 						<img :src="promo.image_url" alt=""/>
@@ -145,7 +145,7 @@
             data: function() {
                 return {
                     selectedDate: null,
-                    processedPromos:[]
+                    processedEvents:[]
                 }
             },
             mounted() {
@@ -162,12 +162,12 @@
                     //sort promos
                     selected = moment(this.selectedDate).format('MM DD YYYY');
                     var vm = this;
-                    vm.processedPromos = _.filter(vm.promotions, function(val){
+                    vm.processedEvents = _.filter(vm.promotions, function(val){
                         start_date = moment(val.start_date).tz(vm.timezone).format('MM DD YYYY');
                         end_date = moment(val.end_date).tz(vm.timezone).format('MM DD YYYY');
                         return moment(selected).isBetween(start_date,end_date, null, '[]');
                     });
-                    console.log(this.processedPromos);
+                    console.log(this.processedEvents);
                     
                     //change active class 
                     old_date = moment(this.selectedDate).date();
@@ -180,7 +180,7 @@
                     var vm = this;
                     var temp_promo = [];
                     var temp_job = [];
-                    _.forEach(this.$store.getters.processedPromos, function(value, key) {
+                    _.forEach(this.$store.getters.processedEvents, function(value, key) {
                         // var current_promo = vm.findPromoById(value);
                         value.description_short = _.truncate(value.description, {
                             'length': 70
