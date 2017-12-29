7c2357b2c1a5195c0491c35153276dbd4c6af7d2
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-29 11:01:47 -0500

    Eastgate Square 2.0 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -104,7 +104,7 @@
                         });
                         temp_promo.push(value);
                     });
-                    _.sortBy(temp_promo, [function(o) { return o.end_date; }]);
+                    _.sortBy(temp_promo, [function(o) { return o.start_date; }]);
                     return temp_promo;
                 },
                 timezone() {
