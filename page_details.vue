049d6032e3f31786c3702e7d70b394b4fe151a5e
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-29 16:22:20 -0500

    Eastgate Square 2.0 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-         <div class="main_container " v-if="currentPage">
+         <div class="page_container " v-if="currentPage">
             <h4 class="page_title promo_main_header" v-html="currentPage.title">  </h4>
             <div class="row padding_30" >
                 <div class="description_text text_left" v-html="currentPage.body">
