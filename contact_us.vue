950b9686066a5425c7bdd3bba263fae17382bae3
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-29 17:13:54 -0500

    Eastgate Square 2.0 1.0  /contact_us.vue

--- contact_us.vue
+++ contact_us.vue
@@ -100,29 +100,29 @@
                     time: new Date()
                 }
             },
-            beforeRouteEnter (to, from, next) {
-                next(vm => {
-                  // access to component instance via `vm`
-                    vm.$store.dispatch('LOAD_PAGE_DATA', {url:vm.property.mm_host + "/pages/canyoncrest-contact.json"}).then(response => {
+            // beforeRouteEnter (to, from, next) {
+            //     next(vm => {
+            //       // access to component instance via `vm`
+            //         vm.$store.dispatch('LOAD_PAGE_DATA', {url:vm.property.mm_host + "/pages/canyoncrest-contact.json"}).then(response => {
                         
-                        vm.currentPage = response.data;
-                        console.log(vm.currentPage);
-                    }, error => {
-                        console.error("Could not retrieve data from server. Please check internet connection and try again.");
-                        vm.$router.replace({ name: '404'});
-                    });
-                })
-            },
-            beforeRouteUpdate (to, from, next) {
-                this.$store.dispatch('LOAD_PAGE_DATA', {url:this.property.mm_host + "/pages/canyoncrest-contact.json"}).then(response => {
-                    // this.dataLoaded = true;
-                    this.currentPage = response.data;
-                    console.log(this.currentPage);
-                }, error => {
-                    console.error("Could not retrieve data from server. Please check internet connection and try again.");
-                    this.$router.replace({ name: '404'});
-                });
-            },
+            //             vm.currentPage = response.data;
+            //             console.log(vm.currentPage);
+            //         }, error => {
+            //             console.error("Could not retrieve data from server. Please check internet connection and try again.");
+            //             vm.$router.replace({ name: '404'});
+            //         });
+            //     })
+            // },
+            // beforeRouteUpdate (to, from, next) {
+            //     this.$store.dispatch('LOAD_PAGE_DATA', {url:this.property.mm_host + "/pages/canyoncrest-contact.json"}).then(response => {
+            //         // this.dataLoaded = true;
+            //         this.currentPage = response.data;
+            //         console.log(this.currentPage);
+            //     }, error => {
+            //         console.error("Could not retrieve data from server. Please check internet connection and try again.");
+            //         this.$router.replace({ name: '404'});
+            //     });
+            // },
             mounted () {
               console.log(this.hours);
               console.log(this.holidayHours);
