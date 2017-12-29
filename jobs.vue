431cced6329d7ecac462f636824801337f87286f
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-29 16:46:48 -0500

    Eastgate Square 2.0 1.0  /jobs.vue

--- jobs.vue
+++ jobs.vue
@@ -188,6 +188,7 @@
                         temp_promo.push(value);
                     });
                     _.sortBy(temp_promo, [function(o) { return o.start_date; }]);
+                    console.log(temp_promo);
                     return temp_promo;
                 },
                 timezone() {
