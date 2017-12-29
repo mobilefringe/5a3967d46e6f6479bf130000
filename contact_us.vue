4bbc9b601737986b6743578bc4dea806cba3513b
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-29 17:11:21 -0500

    Eastgate Square 2.0 1.0  /contact_us.vue

---
+++ contact_us.vue
@@ -0,0 +1,207 @@
+<template>
+    <div class=" main_container" id="promotions_container"> <!-- for some reason if you do not put an outer container div this component template will not render -->
+        <div class="margin_25_across padding_top_40">
+            <div class="row">
+                <div class="col-md-5 col-sm-5">
+                    <div class="col-md-12" v-if="currentPage">
+                        <div class="description_text text_left" v-html="currentPage.body"></div>
+                    </div>
+                </div>
+                <div class="col-md-7 col-sm-7 hidden_phone">
+                    <img style="margin-bottom:20px;" src="//codecloud.cdn.speedyrails.net/sites/5a1f136e6e6f6472c6240000/image/png/1512580269422/placeholder_contact_image.png" alt="plaza">
+                </div> 
+                <!--<vue-datepicker-local v-model="time" type="inline"></vue-datepicker-local>-->
+            </div>
+            <hr/>
+            <div class="row"> 
+                <div class="col-md-12 contact_contents padding_top_20">
+                    <form class="form-horizontal" action="form-submit" @submit.prevent="validateBeforeSubmit">
+                        <div class="form-group ">
+                            <div class="col-sm-6 col-xs-12" :class="{'has-error': errors.has('name')}">
+                                <label class="label" for="name">Name</label>
+                                <input v-model="form_data.name" v-validate="'required|alpha_spaces'" class="form-control" :class="{'input': true}" name="name" type="text" placeholder="Name" data-vv-delay="1000">
+                                <span v-show="errors.has('name')" class="form-control-feedback">{{ errors.first('name') }}</span>
+                            </div>
+                            <div class="col-sm-6 col-xs-12" :class="{'has-error': errors.has('email')}">
+                                <label class="label" for="email">Email</label>
+                                <input v-model="form_data.email" v-validate="'required|email'" class="form-control" :class="{'input': true}" name="email" type="email" placeholder="Email" data-vv-delay="1000">
+                                <span v-show="errors.has('email')" class="form-control-feedback">{{ errors.first('email') }}</span>
+                            </div>
+                        </div>
+                        <!--<div class="form-group">-->
+                            
+                        <!--</div>-->
+                        <div class="form-group">
+                            <div class="col-sm-6 col-xs-12" :class="{'has-error': errors.has('phone')}">
+                                <label class="label" for="phone">Phone</label>
+                                <input v-model="form_data.phone" v-validate="'required|alpha_dash'" class="form-control" :class="{'input': true}" name="phone" type="phone" placeholder="Phone" data-vv-delay="1000">
+                                <span v-show="errors.has('phone')" class="form-control-feedback">{{ errors.first('phone') }}</span>
+                            </div>
+                            <div class="col-sm-6 col-xs-12" :class="{'has-error': errors.has('subject')}">
+                                <label class="label" for="subject">Subject</label>
+                                <input v-model="form_data.subject" v-validate="'required|alpha_spaces'" class="form-control" :class="{'input': true}" name="subject" type="text" placeholder="Subject" data-vv-delay="1000">
+                                <span v-show="errors.has('subject')" class="form-control-feedback">{{ errors.first('subject') }}</span>
+                            </div>
+                        </div>
+                        <!--<div class="form-group ">-->
+                            
+                        <!--</div>-->
+                        <div class="form-group">
+                            <div class="col-xs-12" :class="{'has-error': errors.has('message')}">
+                                <label class="label" for="message">Message</label>
+                                <input v-model="form_data.message" v-validate="'required|alpha_spaces'" class="form-control" :class="{'input': true}" name="message" type="text" placeholder="Message" data-vv-delay="1000">
+                                <span v-show="errors.has('message')" class="form-control-feedback">{{ errors.first('message') }}</span>
+                            </div>
+                        </div>
+                    
+                        <div class="form-group account-btn text-left m-t-10">
+                            <div class="col-xs-12">
+                                <button class="animated_btn" type="submit" :disabled="formSuccess">Submit</button>
+                            </div>
+                        </div>
+                    </form>
+                    
+                    <div id="send_contact_success" class="alert alert-success" role="alert" v-show="formSuccess">
+                        <span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
+                        <span class="sr-only">Success</span>
+                        Thank you for contacting us. A member from our team will contact you shortly.
+                    </div>
+                    <div id="send_contact_error" class="alert alert-danger" role="alert" v-show="formError">
+                        <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+                        <span class="sr-only">Error:</span>
+                        There was an error when trying to submit your request. Please try again later.
+                    </div>
+                    
+                </div>
+            </div>
+            <div class="padding_top_40"></div>    
+        </div>
+    </div>
+</template>
+
+<script>
+    define(["Vue", "moment", "moment-timezone", "vue-moment", "vue-meta", 'vee-validate'], function(Vue, moment, tz, VueMoment, Meta, VeeValidate) {
+        Vue.use(Meta);
+        Vue.use(VeeValidate);
+        return Vue.component("contact-us-component", {
+            template: template, // the variable template will be injected
+            data: function() {
+                return {
+                    dayOfTheWeek : [
+                        "Sunday", "Monday","Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+                    ],
+                    currentPage: null,
+                    form_data : {},
+                    loginPending: null,
+                    formSuccess : false,
+                    formError: false,
+                    time: new Date()
+                }
+            },
+            beforeRouteEnter (to, from, next) {
+                next(vm => {
+                  // access to component instance via `vm`
+                    vm.$store.dispatch('LOAD_PAGE_DATA', {url:vm.property.mm_host + "/pages/canyoncrest-contact.json"}).then(response => {
+                        
+                        vm.currentPage = response.data;
+                        console.log(vm.currentPage);
+                    }, error => {
+                        console.error("Could not retrieve data from server. Please check internet connection and try again.");
+                        vm.$router.replace({ name: '404'});
+                    });
+                })
+            },
+            beforeRouteUpdate (to, from, next) {
+                this.$store.dispatch('LOAD_PAGE_DATA', {url:this.property.mm_host + "/pages/canyoncrest-contact.json"}).then(response => {
+                    // this.dataLoaded = true;
+                    this.currentPage = response.data;
+                    console.log(this.currentPage);
+                }, error => {
+                    console.error("Could not retrieve data from server. Please check internet connection and try again.");
+                    this.$router.replace({ name: '404'});
+                });
+            },
+            mounted () {
+              console.log(this.hours);
+              console.log(this.holidayHours);
+            },
+            computed: {
+                timezone () {
+                  return this.$store.getters.getTimezone;
+                },
+                property (){
+                    return this.$store.getters.getProperty;
+                },
+                hours () {
+                    return this.$store.getters.getPropertyHours;
+                },
+                holidayHours () {
+                    return this.$store.getters.getPropertyHolidayHours;
+                },
+                reducedHolidays () {
+                    var holidayHours = this.holidayHours;
+                    return _.filter(holidayHours, function(o) { return !o.is_closed; });
+                },
+                closeHolidays () {
+                    var holidayHours = this.holidayHours;
+                    return _.filter(holidayHours, function(o) { return o.is_closed; });
+                },
+                rannumber () {
+                    var rannumber='';
+                    for(ranNum=1; ranNum<=6; ranNum++){
+                      rannumber+=Math.floor(Math.random()*10).toString();
+                    }
+                    return rannumber;
+                }
+            },
+            methods: {
+                validateBeforeSubmit() {
+                    this.$validator.validateAll().then((result) => {
+                        if (result) {
+                            let errors = this.errors;
+                            console.log("sending form data", this.form_data);
+                            send_data = {};
+                            // send_data.url = '/api/v1/contact_us';
+                            send_data.form_data = JSON.stringify(this.serializeObject(this.form_data));
+                            this.$store.dispatch("CONTACT_US", send_data).then(res => {
+                                // this.$router.replace({
+                                //     name: 'home'
+                                // })
+                                this.formSuccess = true;
+                            }).catch(error => {
+                                try {
+                                    if (error.response.status == 401) {
+                                        console.log("Data load error: " + error.message);
+                                        this.formError = true;
+                                    } 
+                                    else {
+                                        console.log("Data load error: " + error.message);
+                                        this.formError = true;
+                                    }
+                                } 
+                                catch (e) {
+                                    console.log("Data load error: " + error.message);
+                                    this.formError = true;
+                                }
+                            })
+                        }
+                    })
+                },
+                serializeObject (obj) {
+                    console.log(obj);
+                    var newObj = [];
+                    // var counter = 0;
+                    _.forEach(obj, function(value, key) {
+                        var tempVal = {};
+                        tempVal.name = key;
+                        tempVal.value = value;
+                        // console.log(key);
+                        // counter ++;
+                        newObj.push(tempVal);
+                    });
+                    return newObj;
+                }
+            }
+        });
+    });
+</script>
