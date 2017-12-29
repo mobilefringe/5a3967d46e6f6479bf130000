ddbd40f85cbc6cd0315022adb6cb2b9007270a96
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-29 13:16:53 -0500

    Eastgate Square 2.0 1.0  /event_details.vue

--- event_details.vue
+++ event_details.vue
@@ -138,19 +138,19 @@
                 },
                 currentEvent: function() {
                     var vm = this;
-            var temp_promo = [];
-            var current_id =_.toNumber(this.currentEvent.id);
-            console.log(current_id);
-            _.forEach(this.allEvents, function(value, key) {
-                console.log(value)
-                if(_.toNumber(value.id) != current_id){
-                    var current_promo = vm.findEventById(value.id);
-                    current_promo.description_short = _.truncate(current_promo.description, {'length': 70});
-                    temp_promo.push(current_promo);
-                }
-            });
-            this.storePromos = temp_promo;
-            console.log("promos",this.storePromos);
+                    var temp_promo = [];
+                    var current_id =_.toNumber(this.currentEvent.id);
+                    console.log(current_id);
+                    _.forEach(this.allEvents, function(value, key) {
+                        console.log(value)
+                        if(_.toNumber(value.id) != current_id){
+                            var current_promo = vm.findEventById(value.id);
+                            current_promo.description_short = _.truncate(current_promo.description, {'length': 70});
+                            temp_promo.push(current_promo);
+                        }
+                    });
+                    this.storePromos = temp_promo;
+                    console.log("promos",this.storePromos);
                 }
             },
             computed: {
