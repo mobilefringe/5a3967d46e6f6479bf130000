5b3e93e58bcbb338611bcd87a9e888fbd08f8399
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-29 17:24:58 -0500

    Eastgate Square 2.0 1.0  /contact_us.vue

--- contact_us.vue
+++ contact_us.vue
@@ -10,7 +10,7 @@
                     </div>
                 </div>
                 <div class="col-md-7 col-sm-7 hidden_phone">
-                    <img style="margin-bottom:20px;" src="//codecloud.cdn.speedyrails.net/sites/5a1f136e6e6f6472c6240000/image/png/1512580269422/placeholder_contact_image.png" alt="plaza">
+                    <!--<img style="margin-bottom:20px;" src="//codecloud.cdn.speedyrails.net/sites/5a1f136e6e6f6472c6240000/image/png/1512580269422/placeholder_contact_image.png" alt="plaza">-->
                 </div> 
                 <!--<vue-datepicker-local v-model="time" type="inline"></vue-datepicker-local>-->
             </div>
