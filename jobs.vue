51aaef2f83925ab7397380a5cb58847f2db6e33b
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-29 16:45:56 -0500

    Eastgate Square 2.0 1.0  /jobs.vue

--- jobs.vue
+++ jobs.vue
@@ -14,7 +14,7 @@
 			<div class="row promo_dets is-table-row" v-for="promo in jobs">
 				<div class="col-sm-7" >
 					<div class="promo_div_image">
-						<img :src="promo.image_url" alt=""/>
+						<img :src="promo.store.job" alt=""/>
 					</div>
 				</div>
 				<div class="col-sm-5 promo_div_dets text-left">
