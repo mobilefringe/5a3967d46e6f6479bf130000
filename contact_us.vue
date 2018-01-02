f6cca6230bceab715f722dcbb8731499d94774e4
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-02 10:41:43 -0500

    Eastgate Square 2.0 1.0  /contact_us.vue

--- contact_us.vue
+++ contact_us.vue
@@ -188,12 +188,8 @@
                             let errors = this.errors;
                             console.log("sending form data", this.form_data);
                             send_data = {};
-                            // send_data.url = '/api/v1/contact_us';
                             send_data.form_data = JSON.stringify(this.serializeObject(this.form_data));
                             this.$store.dispatch("CONTACT_US", send_data).then(res => {
-                                // this.$router.replace({
-                                //     name: 'home'
-                                // })
                                 this.formSuccess = true;
                             }).catch(error => {
                                 try {
