12051d2d3e3d8d518a409814b7c81272b1b2bbce
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-03 17:13:17 -0500

    Eastgate Square 2.0 1.0  /contact_us.vue

--- contact_us.vue
+++ contact_us.vue
@@ -183,7 +183,7 @@
                             //creating random validation num 
                             console.log(this.errors);
                             this.correctValNum = this.rannumber;
-                            this.errors.push({field:"validate",id:"6",msg:"Please enter correct number"});
+                            this.errors.items.push({field:"validate",id:"6",msg:"Please enter correct number"});
                             console.log(this.errors);
                         }
                 },
