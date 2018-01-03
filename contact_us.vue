fc94637fa5aa0706ad6818d36ef05e1abfc430e5
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-03 16:17:46 -0500

    Eastgate Square 2.0 1.0  /contact_us.vue

--- contact_us.vue
+++ contact_us.vue
@@ -152,30 +152,31 @@
                 validateBeforeSubmit() {
                     this.$validator.validateAll().then((result) => {
                         if(correctValNum === validaNum) {
-                        if (result) {
-                            let errors = this.errors;
-                            console.log("sending form data", this.form_data);
-                            send_data = {};
-                            send_data.form_data = JSON.stringify(this.serializeObject(this.form_data));
-                            console.log(send_data.form_data);
-                            this.$store.dispatch("CONTACT_US", send_data).then(res => {
-                                this.formSuccess = true;
-                            }).catch(error => {
-                                try {
-                                    if (error.response.status == 401) {
-                                        console.log("Data load error: " + error.message);
-                                        this.formError = true;
+                            if (result) {
+                                let errors = this.errors;
+                                console.log("sending form data", this.form_data);
+                                send_data = {};
+                                send_data.form_data = JSON.stringify(this.serializeObject(this.form_data));
+                                console.log(send_data.form_data);
+                                this.$store.dispatch("CONTACT_US", send_data).then(res => {
+                                    this.formSuccess = true;
+                                }).catch(error => {
+                                    try {
+                                        if (error.response.status == 401) {
+                                            console.log("Data load error: " + error.message);
+                                            this.formError = true;
+                                        } 
+                                        else {
+                                            console.log("Data load error: " + error.message);
+                                            this.formError = true;
+                                        }
                                     } 
-                                    else {
+                                    catch (e) {
                                         console.log("Data load error: " + error.message);
                                         this.formError = true;
                                     }
-                                } 
-                                catch (e) {
-                                    console.log("Data load error: " + error.message);
-                                    this.formError = true;
-                                }
-                            })
+                                })
+                            }
                         }
                     })
                 },
