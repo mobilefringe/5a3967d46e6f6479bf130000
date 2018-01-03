65bc1d1e321797ff4e389473f71bfddd26a2f5af
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-03 15:15:32 -0500

    Eastgate Square 2.0 1.0  /main.js

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ require.config({
         'vue-select': 'https://cdn.jsdelivr.net/npm/vue-select@2.3.0/dist/vue-select.min',
         'vue-social-sharing': 'https://cdn.jsdelivr.net/npm/vue-social-sharing@2.3.1/dist/vue-social-sharing.min',
         'vee-validate' : 'https://cdn.jsdelivr.net/npm/vee-validate@latest/dist/vee-validate',
-        'vue-grecaptcha': 'https://unpkg.com/vue-grecaptcha@latest/dist/vue-grecaptcha'
+        'vue-grecaptcha': 'https://unpkg.com/vue-grecaptcha@1.0.1/dist/vue-grecaptcha'
     }
 });
 
