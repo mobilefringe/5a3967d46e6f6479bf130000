cbc5105efa2f3c6a5618dbec0d33b48eea8e14dd
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-03 15:52:31 -0500

    Eastgate Square 2.0 1.0  /main.js

--- main.js
+++ main.js
@@ -36,7 +36,9 @@ require(['Vue', 'vue2-filters', 'vue_router', 'routes', 'store', 'vue-i18n', 'lo
     Vue.use(VueI18n);
     Vue.use(SocialSharing);
     // console.log("VueGrecaptcha", VueGrecaptcha);
-    Vue.use(VueGrecaptcha);
+    // Vue.use(VueGrecaptcha, {
+    //     sitekey: '6Ld7MD8UAAAAAJBuiMVV5YaWsLGpdjq5pcAdvlNW'
+    // });
     // Vue.component('vue-grecaptcha', VueGrecaptcha);
     /* initialize router */
     const router = new VueRouter({
