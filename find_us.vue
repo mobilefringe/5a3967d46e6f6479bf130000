85c14e56bf397c18a7b6447d34f821fa7e871591
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-04 11:41:40 -0500

    Eastgate Square 2.0 1.0  /find_us.vue

--- find_us.vue
+++ find_us.vue
@@ -5,7 +5,7 @@
 			<div class="row text-left">
 					<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1700.7834802639834!2d-79.76570131628588!3d43.230419353327626!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xc2187d635df8645b!2sEastgate+Square+Fortinos!5e0!3m2!1sen!2sca!4v1515083028714" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 					<div class="page_header all_caps double_border_bottom">
-            			<div class="page_container text_left"> <h2>How to Get Here</h2></div>
+            			<div class="page_container text_left">How to Get Here</div>
             		</div>
 					
 					<p>Eastgate Square is easy to find and there is plenty of parking once you arrive!</p>
@@ -15,7 +15,7 @@
 					<p>Take the Queenston Rd. exit and head eastbound. You will find Eastgate Square on the left hand side, just before the intersection with Centennial Pkwy N.</p>
 					
 					<div class="page_header all_caps double_border_bottom">
-            			<div class="page_container text_left"> <h2>Public Transit</h2></div>
+            			<div class="page_container text_left"> Public Transit</div>
             		</div>
 					<p>Eastgate Square is serviced by the Hamilton Street Railway. The following Routes will get you here: 1 (King), B-Line Express, 55 (Stoney Creek Central), 56 (Centennial), and 58 (Stoney Creek Local). Visit <a href="//www.hamilton.ca/CityServices/transit/">www.hamilton.ca/CityServices/transit/</a> for bus maps and schedules.</p>
 			</div>
