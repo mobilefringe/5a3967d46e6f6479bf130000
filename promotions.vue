ee76e655cb2e6af7e00eaa5f74246845fea05ea0
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-04 13:19:12 -0500

    Eastgate Square 2.0 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="page_container promotions_container"><!-- for some reason if you do not put an outer container div this component template will not render -->
 		<div class="date_bar">
-		    <span @click="beforeDate"> <i class="fa fa-chevron-left"></i></span>
+		    <span @click="beforeDate" class="some_class"> <i class="fa fa-chevron-left"></i></span>
 		    <span class="current_date">
 		        {{selectedDate}}
 		    </span>
