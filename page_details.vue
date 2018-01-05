9aa490f30af122b51478863eb49102cb63e450ca
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-05 17:10:57 -0500

    Eastgate Square 2.0 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="pages_contaienr" v-if="currentPage">
+    <div class="pages_container" v-if="currentPage">
         <div class="page_header all_caps double_border_bottom">
 			<div class="page_container text_left" v-html="currentPage.title"> </div>
 		</div>
