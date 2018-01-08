cc712b30e478b70cba171abfd0079c62ec39c39f
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-08 09:57:58 -0500

    Eastgate Square 2.0 1.0  /photos.vue

---
+++ photos.vue
@@ -0,0 +1,3 @@
+<!--<a :href="sectionThree.image_url" :data-lightbox="sectionThree.name">-->
+<!--                    		        <h5>View Larger&nbsp;<i class="fa fa-angle-double-right" aria-hidden="true"></i></h5>-->
+<!--                		        </a>-->
