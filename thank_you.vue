2f983ff602521c382b240bdcaf8aa4bdd8790700
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-08 16:38:02 -0500

    Eastgate Square 2.0 1.0  /thank_you.vue

---
+++ thank_you.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="page_container">
+        Your subscription has been confirmed. You've been added to our list and will hear from us soon.
+    </div>
+</template>
+
+<script>
+  define(["Vue"], function(Vue) {
+    Vue.use(Meta);
+    return Vue.component("thank-you-component", {
+      template: template, // the variable template will be injected
+      data: function() {
+        return {
+            
+        }
+      }
+    //   beforeRouteEnter (to, from, next) {
+    //     next(vm => {
+    //       // access to component instance via `vm`
+    //       vm.meta = vm.findMetaDataByPath(to.path);
+    //     })
+    //   },
+    //   beforeRouteUpdate (to, from, next) {
+    //     this.meta = this.findMetaDataByPath(to.path);
+    //     next();
+    //   },
+    //   computed: {
+    //     findMetaDataByPath () {
+    //       return this.$store.getters.findMetaDataByPath;
+    //     }
+    //   },
+    //   metaInfo () {
+    //     return {
+    //       title: this.meta.meta_title,
+    //       meta: [
+    //         {name: 'description', content: this.meta.meta_description},
+    //         {name: 'keywords', content: this.meta.meta_keywords}
+    //       ]
+    //     }
+    //   }
+    });
+  });
+</script>
