7da56bcb693ef8320a7e846ba563a53d4a5b2633
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-08 11:08:51 -0500

    Eastgate Square 2.0 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -124,7 +124,16 @@
                         this.processedStores = filtered;
                     }
                     // this.processedStores = 
-                }
+                },
+                updateSVGMap (map) {
+                    this.map = map;
+                },
+                dropPin(store) {
+                    this.svgMapRef.hideMarkers();
+                    console.log(store);
+                    this.svgMapRef.addMarker(store,'//codecloud.cdn.speedyrails.net/sites/589e308f6e6f641b9f010000/image/png/1484850466000/show_pin.png');
+                    this.svgMapRef.setViewBox(store)
+                },
             },
             computed: {
                 storesByAlphaIndex() {
