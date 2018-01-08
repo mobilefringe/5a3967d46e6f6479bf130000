7683cf7446630b5703df824be98a3a5c8bbd8942
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-08 09:58:26 -0500

    Eastgate Square 2.0 1.0  /photos.vue

--- photos.vue
+++ photos.vue
@@ -1,3 +1,49 @@
 <!--<a :href="sectionThree.image_url" :data-lightbox="sectionThree.name">-->
 <!--                    		        <h5>View Larger&nbsp;<i class="fa fa-angle-double-right" aria-hidden="true"></i></h5>-->
 <!--                		        </a>-->
+
+<template>
+  <router-view></router-view>
+</template>
+
+<script>
+  define(["Vue", "vue-meta"], function(Vue, Meta) {
+    Vue.use(Meta);
+    return Vue.component("default-component", {
+      template: template, // the variable template will be injected
+      data: function() {
+        return {
+          meta: {
+            meta_title: "",
+            meta_description: "",
+            meta_keywords: ""
+          }
+        }
+      },
+      beforeRouteEnter (to, from, next) {
+        next(vm => {
+          // access to component instance via `vm`
+          vm.meta = vm.findMetaDataByPath(to.path);
+        })
+      },
+      beforeRouteUpdate (to, from, next) {
+        this.meta = this.findMetaDataByPath(to.path);
+        next();
+      },
+      computed: {
+        findMetaDataByPath () {
+          return this.$store.getters.findMetaDataByPath;
+        }
+      },
+      metaInfo () {
+        return {
+          title: this.meta.meta_title,
+          meta: [
+            {name: 'description', content: this.meta.meta_description},
+            {name: 'keywords', content: this.meta.meta_keywords}
+          ]
+        }
+      }
+    });
+  });
+</script>
