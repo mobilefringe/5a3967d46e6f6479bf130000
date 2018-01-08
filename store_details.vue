08040bf2b392fc415e12e3dd5cd152cc57050323
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-08 15:32:31 -0500

    Eastgate Square 2.0 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -182,7 +182,6 @@
                 },
                 getPNGurl () {
                     return "https://www.mallmaverick.com" + this.property.map_url;
-                    // return "//www.mallmaverick.com/system/site_images/photos/000/035/014/original/Canyon_Crest_-_Map.svg?1512066588";
                 },
                 svgMapRef() {
                     return _.filter(this.$children, function(o) {
