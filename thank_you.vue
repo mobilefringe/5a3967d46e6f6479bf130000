500a29b5e11f6f028c60c57a08f790d2faf7bab9
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-08 16:39:35 -0500

    Eastgate Square 2.0 1.0  /thank_you.vue

--- thank_you.vue
+++ thank_you.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="page_container">
-        Your subscription has been confirmed. You've been added to our list and will hear from us soon.
+        <div>
+            Your subscription has been confirmed. You've been added to our list and will hear from us soon.
+        </div>
     </div>
 </template>
 
