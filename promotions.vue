d0cf9ac040373d46c32e9d2b6da5b2703b279778
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-16 14:40:47 -0500

    Eastgate Square 2.0 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -159,9 +159,9 @@
                   console.error("Could not retrieve data from server. Please check internet connection and try again.");
                 });
                 
-                var date = moment(this.selectedDate).date();
-                console.log("date is", date);
-                $("#date_"+ date).addClass('active');
+                // var date = moment(this.selectedDate).date();
+                // console.log("date is", date);
+                // $("#date_"+ date).addClass('active');
                 // $("#date_29").addClass('active');
             },
             watch: {
