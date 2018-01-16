3d5702ba1af0c79ce48bddd5edbb376bb2220ebf
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-16 14:37:16 -0500

    Eastgate Square 2.0 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -153,9 +153,9 @@
                 this.$store.dispatch("getData", "promotions").then(response => {
                     this.dataloaded = true;
                     this.selectedDate = moment().tz(this.timezone).format('MMM D, YYYY');
-                console.log(this.selectedDate);
-                var date = moment(this.selectedDate).date();
-                console.log("date is", date);
+                    console.log(this.selectedDate);
+                    var date = moment(this.selectedDate).date();
+                    console.log("date is", date);
                 }, error => {
                   console.error("Could not retrieve data from server. Please check internet connection and try again.");
                 });
