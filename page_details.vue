6d8147cae9d6e8d94421e973ba572264f45dfa0a
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-16 13:29:50 -0500

    Eastgate Square 2.0 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -35,37 +35,38 @@
                     currentPage: null
                 }
             },
-            beforeRouteEnter(to, from, next) {
-                next(vm => {
-                    // access to component instance via `vm`
-                    vm.$store.dispatch('LOAD_PAGE_DATA', {
-                        url: vm.property.mm_host + "/pages/" + to.params.id + ".json"
-                    }).then(response => {
+            props:['id'],
+            // beforeRouteEnter(to, from, next) {
+            //     next(vm => {
+            //         // access to component instance via `vm`
+            //         vm.$store.dispatch('LOAD_PAGE_DATA', {
+            //             url: vm.property.mm_host + "/pages/" + to.params.id + ".json"
+            //         }).then(response => {
 
-                        vm.currentPage = response.data;
-                        console.log(vm.currentPage);
-                    }, error => {
-                        console.error("Could not retrieve data from server. Please check internet connection and try again.");
-                        vm.$router.replace({
-                            name: '404'
-                        });
-                    });
-                })
-            },
-            beforeRouteUpdate(to, from, next) {
-                this.$store.dispatch('LOAD_PAGE_DATA', {
-                    url: this.property.mm_host + "/pages/" + to.params.id + ".json"
-                }).then(response => {
-                    // this.dataLoaded = true;
-                    this.currentPage = response.data;
-                    console.log(this.currentPage);
-                }, error => {
-                    console.error("Could not retrieve data from server. Please check internet connection and try again.");
-                    this.$router.replace({
-                        name: '404'
-                    });
-                });
-            },
+            //             vm.currentPage = response.data;
+            //             console.log(vm.currentPage);
+            //         }, error => {
+            //             console.error("Could not retrieve data from server. Please check internet connection and try again.");
+            //             vm.$router.replace({
+            //                 name: '404'
+            //             });
+            //         });
+            //     })
+            // },
+            // beforeRouteUpdate(to, from, next) {
+            //     this.$store.dispatch('LOAD_PAGE_DATA', {
+            //         url: this.property.mm_host + "/pages/" + to.params.id + ".json"
+            //     }).then(response => {
+            //         // this.dataLoaded = true;
+            //         this.currentPage = response.data;
+            //         console.log(this.currentPage);
+            //     }, error => {
+            //         console.error("Could not retrieve data from server. Please check internet connection and try again.");
+            //         this.$router.replace({
+            //             name: '404'
+            //         });
+            //     });
+            // },
             computed: {
                 timezone() {
                     return this.$store.getters.getTimezone;
