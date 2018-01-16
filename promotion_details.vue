3b5cf653ba62aa63328debeeecd1e9577c978d83
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-16 13:29:37 -0500

    Eastgate Square 2.0 1.0  /promotion_details.vue

--- promotion_details.vue
+++ promotion_details.vue
@@ -123,27 +123,27 @@
                 }
             },
             props:['id'],
-            beforeRouteEnter(to, from, next) {
-                next(vm => {
-                    // access to component instance via `vm`
-                    vm.currentPromo = vm.findPromoBySlug(to.params.id);
-                    console.log(vm.currentPromo);
-                    if (vm.currentPromo === null || vm.currentPromo === undefined) {
-                        vm.$router.replace({
-                            name: '404'
-                        });
-                    }
-                })
-            },
-            beforeRouteUpdate(to, from, next) {
-                this.currentPromo = this.findPromoBySlug(to.params.id);
-                console.log(this.currentPromo);
-                if (this.currentPromo === null || this.currentPromo === undefined) {
-                    this.$router.replace({
-                        name: '404'
-                    });
-                }
-            },
+            // beforeRouteEnter(to, from, next) {
+            //     next(vm => {
+            //         // access to component instance via `vm`
+            //         vm.currentPromo = vm.findPromoBySlug(to.params.id);
+            //         console.log(vm.currentPromo);
+            //         if (vm.currentPromo === null || vm.currentPromo === undefined) {
+            //             vm.$router.replace({
+            //                 name: '404'
+            //             });
+            //         }
+            //     })
+            // },
+            // beforeRouteUpdate(to, from, next) {
+            //     this.currentPromo = this.findPromoBySlug(to.params.id);
+            //     console.log(this.currentPromo);
+            //     if (this.currentPromo === null || this.currentPromo === undefined) {
+            //         this.$router.replace({
+            //             name: '404'
+            //         });
+            //     }
+            // },
             watch: {
                 map: function() {
                     var vm = this;
