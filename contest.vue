dfa89758dda4f95674e8acb188315ee0fc6ee4ee
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-17 14:27:30 -0500

    Eastgate Square 2.0 1.0  /contest.vue

--- contest.vue
+++ contest.vue
@@ -36,6 +36,11 @@
 								<input v-model="form_data.phone" v-validate="'required:true'" class="form-control" :class="{'input': true}" name="phone" type="text" placeholder="Phone number" data-vv-delay="500">
 								<span v-show="errors.has('phone')" class="form-control-feedback">{{ errors.first('phone') }}</span>
 							</div>
+							<div class="col-sm-6 col-xs-12" :class="{'has-error': errors.has('phone')}">
+								<label class="label" for="phone">Phone Number</label>
+								<input v-model="form_data.phone" v-validate="'required:true'" class="form-control" :class="{'input': true}" name="phone" type="text" placeholder="Phone number" data-vv-delay="500">
+								<span v-show="errors.has('phone')" class="form-control-feedback">{{ errors.first('phone') }}</span>
+							</div>
 							<div class="col-sm-6 col-xs-12 no_padding" >
 								<div class="col-xs-12" :class="{'has-error': errors.has('street')}">
 									<label class="label" for="street">Street Address</label>
