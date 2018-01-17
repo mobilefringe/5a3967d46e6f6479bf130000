b2337b30309c39f6acfeeb240da04116e4ce95e8
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-17 11:50:54 -0500

    Eastgate Square 2.0 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -117,6 +117,7 @@
                     new_features.map(feature => {
                         feature.short_description = _.truncate(feature.description,{'length': 50});
                         feature.image_url = temp_images[i];
+                         i ++;
                     });
                     return new_features;
                 }
