8cc3b114077615b6d919cf663c19790918aea10a
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-17 14:18:42 -0500

    Eastgate Square 2.0 1.0  /contest.vue

--- contest.vue
+++ contest.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="page_container" id="contact_us_container" v-if="currentContest"> <!-- for some reason if you do not put an outer container div this component template will not render -->
-        <div class="margin_25_across padding_top_40">
+        <div>
             <div class="row">
                     <img :src='currentContest.image_url' alt="contest image">
                 </div> 
