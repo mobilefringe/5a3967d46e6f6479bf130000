78bc7525a9981de9ce3006ad85d54c14f7182e84
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-17 14:28:16 -0500

    Eastgate Square 2.0 1.0  /contest.vue

--- contest.vue
+++ contest.vue
@@ -41,10 +41,10 @@
 								<input v-model="form_data.street" v-validate="'required:true'" class="form-control" :class="{'input': true}" name="street" type="text" placeholder="Street Address" data-vv-delay="500">
 								<span v-show="errors.has('street')" class="form-control-feedback">{{ errors.first('street') }}</span>
 							</div>
-							<div class="col-sm-6 col-xs-12" :class="{'has-error': errors.has('street')}">
-								<label class="label" for="street">Street Address</label>
-								<input v-model="form_data.street" v-validate="'required:true'" class="form-control" :class="{'input': true}" name="street" type="text" placeholder="Street Address" data-vv-delay="500">
-								<span v-show="errors.has('street')" class="form-control-feedback">{{ errors.first('street') }}</span>
+							<div class="col-sm-6 col-xs-12"  :class="{'has-error': errors.has('city')}" style="padding-top: 20px;">
+								<label class="label" for="city">Town/ City</label>
+								<input v-model="form_data.city" v-validate="'required:true'" class="form-control" :class="{'input': true}" name="city" type="text" placeholder="Town/City" data-vv-delay="500">
+								<span v-show="errors.has('city')" class="form-control-feedback">{{ errors.first('city') }}</span>
 							</div>
 							<div class="col-sm-6 col-xs-12 no_padding" >
 								<div class="col-xs-12" :class="{'has-error': errors.has('street')}">
