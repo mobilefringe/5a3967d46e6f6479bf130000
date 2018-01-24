cf2d2a63c33579fb3d292731337770936daae3e6
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-24 11:40:42 -0500

    Eastgate Square 2.0 1.0  /utility.js

--- utility.js
+++ utility.js
@@ -1,27 +1,27 @@
 define([], function () {
-  return {
-    rannumber() {
-        var randnumber = '';
-        for (ranNum = 1; ranNum <= 6; ranNum++) {
-            randnumber += Math.floor(Math.random() * 10).toString();
+    return {
+        rannumber() {
+            var randnumber = '';
+            for (ranNum = 1; ranNum <= 6; ranNum++) {
+                randnumber += Math.floor(Math.random() * 10).toString();
+            }
+            return randnumber;
+        },
+        serializeObject(obj) {
+            console.log(obj);
+            var newObj = [];
+            // var counter = 0;
+            _.forEach(obj, function(value, key) {
+                var tempVal = {};
+                tempVal.name = key;
+                tempVal.value = value;
+                // console.log(key);
+                // counter ++;
+                newObj.push(tempVal);
+            });
+            return newObj;
         }
-        return randnumber;
-    },
-    serializeObject(obj) {
-        console.log(obj);
-        var newObj = [];
-        // var counter = 0;
-        _.forEach(obj, function(value, key) {
-            var tempVal = {};
-            tempVal.name = key;
-            tempVal.value = value;
-            // console.log(key);
-            // counter ++;
-            newObj.push(tempVal);
-        });
-        return newObj;
     }
-  }
 });
 
 
