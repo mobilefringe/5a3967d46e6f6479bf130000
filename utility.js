360f1d28cdd80a3c7d1b52f24ca6113c7e5f3fa3
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-24 10:29:49 -0500

    Eastgate Square 2.0 1.0  /utility.js

--- utility.js
+++ utility.js
@@ -1,12 +1,12 @@
 define(['json!en-ca.json', 'json!fr-ca.json'], function (en, fr) {
   return {
     rannumber() {
-    var rannumber = '';
-    for (ranNum = 1; ranNum <= 6; ranNum++) {
-        rannumber += Math.floor(Math.random() * 10).toString();
+        var randnumber = '';
+        for (ranNum = 1; ranNum <= 6; ranNum++) {
+            randnumber += Math.floor(Math.random() * 10).toString();
+        }
+        return randnumber;
     }
-    return rannumber;
-}
   }
 });
 
