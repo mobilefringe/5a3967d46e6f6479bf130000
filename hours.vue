8c0aae9d5468784b00d5e62578389c032b99f8b1
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-25 12:54:31 -0500

    Eastgate Square 2.0 1.0  /hours.vue

--- hours.vue
+++ hours.vue
@@ -70,6 +70,7 @@
                 },
                 reducedHolidays () {
                     var holidayHours = this.holidayHours;
+                    console.log(holidayHours);
                     return _.sortBy(_.filter(holidayHours, function(o) { return !o.is_closed; }), [function(o) { return o.start_date; }]);
                 },
                 closeHolidays () {
