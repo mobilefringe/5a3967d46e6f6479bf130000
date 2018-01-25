6fd953fb2f3421eb502c635104488b081853b755
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-24 21:29:49 -0500

    Eastgate Square 2.0 1.0  /promotion_details.vue

--- promotion_details.vue
+++ promotion_details.vue
@@ -20,7 +20,7 @@
 			</div>
 			<div class="col-sm-8 promo_desc_container">
 				<div class="text-left promo_description">
-				<p v-html="currentPromo.rich_description"></p>
+				    <p v-html="currentPromo.rich_description"></p>
 				</div>
 			</div>
 		</div>
@@ -36,13 +36,12 @@
 		            <p class="promo_div_name">{{promo.name}}</p>
 		            <p class="promo_div_promo_name">{{promo.store.name | uppercase}}</p>
 		            <p class="promo_div_date">{{promo.start_date | moment("MMM D", timezone)}} - {{promo.end_date | moment("MMM D", timezone)}}</p>
-		            
-						<p class="promo_div_description">{{promo.description_short}}</p>
-						<span class="feature_read_more">
-							<router-link :to="'/promotions/'+promo.slug" class="mobile_readmore" >
-								<p class="feature-readmore">Read More <i class="fa fa-chevron-right pull-right" aria-hidden="true"></i></p>
-							</router-link>
-						</span>
+		            <p class="promo_div_description">{{promo.description_short}}</p>
+					<span class="feature_read_more">
+						<router-link :to="'/promotions/'+promo.slug" class="mobile_readmore" >
+							<p class="feature-readmore">Read More <i class="fa fa-chevron-right pull-right" aria-hidden="true"></i></p>
+						</router-link>
+					</span>
 		        </div>
 		    </div>
 		</div>
