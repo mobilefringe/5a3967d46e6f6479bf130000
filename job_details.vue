60c03e8232a5aabf9a8f5ae127ea83972e5e76cc
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-29 15:23:59 -0500

    Eastgate Square 2.0 1.0  /job_details.vue

--- job_details.vue
+++ job_details.vue
@@ -141,9 +141,7 @@
                 updateCurrentJob(id) {
                     this.currentJob = this.findJobBySlug(id);
                     if (this.currentJob === null || this.currentJob === undefined) {
-                        this.$router.replace({
-                            name: '404'
-                        });
+                        this.$router.replace({ path: '/'});
                     }
                 }
             }
