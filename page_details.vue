1938ba57cfb2a183162086998dd56ae5237f428f
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-01-29 14:34:27 -0500

    Eastgate Square 2.0 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -37,8 +37,9 @@
             },
             props:['id'],
             beforeRouteUpdate(to, from, next) {
+                host_name = this.property.mm_host.replace("http:", "");
                 this.$store.dispatch('LOAD_PAGE_DATA', {
-                    url: this.property.mm_host + "/pages/" + to.params.id + ".json"
+                    url: host_name + "/pages/" + to.params.id + ".json"
                 }).then(response => {
                     this.currentPage = response.data;
                 }, error => {
