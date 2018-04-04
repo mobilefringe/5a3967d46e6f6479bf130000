7b5778a0f35f229f4b317f453756ada57a094924
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-04-04 12:29:41 -0400

    Eastgate Square 2.0 1.0  /header.vue

---
+++ header.vue
@@ -0,0 +1,47 @@
+<template>
+    
+</template>
+
+<script>
+    define(["Vue", "vuex", 'vue!social_links.vue', "bootstrap-vue", 'json!menu_items.json'], function (Vue, Vuex, SocialLinks, BootstrapVue, MenuItems) {
+        Vue.use(BootstrapVue);
+        return Vue.component("header-component", {
+            template: template, // the variable template will be injected,
+            data: function() {
+                return {
+                    dataLoaded: false,
+                    show_mobile_menu: false,
+                    property_logo: "//codecloud.cdn.speedyrails.net/sites/5a9714046e6f644dc3160000/image/png/1520457420000/whitelogo1@2x.png",
+                    menu_items: MenuItems,
+                }
+            },
+            watch: {
+                $route: function() {
+                    // hide dropdown when route changes
+                    _.forEach(this.menu_items, function(value, key) {
+                        value.show_sub_menu = false;
+                    });
+                    this.show_mobile_menu = false; //close menu when navigating to new page
+                },
+                show_mobile_menu: function() {
+                    if(this.show_mobile_menu === true){
+                        document.body.classList.add("no-scroll");
+                    } else if (this.show_mobile_menu === false) {
+                        document.body.classList.remove("no-scroll");
+                    }
+                }
+            },
+            computed: {
+                ...Vuex.mapGetters([
+                    'property',
+                    'timezone',
+                    'getTodayHours'
+                ]),
+                todays_hours() {
+                    return this.getTodayHours;
+                }
+            },
+            
+        });
+    });
+</script>
