65eba4e91720d181d7ba2e8bc3b42355c5c22286
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-07 14:54:59 -0400

    Eastgate Square 2.0 1.1  /events.vue

--- events.vue
+++ events.vue
@@ -12,12 +12,12 @@
 					<p class="promo_div_date">{{promo.start_date | moment("MMM D", timezone)}} - {{promo.end_date | moment("MMM D", timezone)}}</p>
 					<p class="promo_div_description">{{promo.description_short}}</p>
 					<div class="promo_feature_share row is-table-row">
-    					<span class="feature_read_more col-sm-6">
+    					<span class="feature_read_more col-sm-8">
     						<router-link :to="'/events/'+promo.slug" class="mobile_readmore" >
     							<p class="feature-readmore" :aria="promo.name">View Event Details<i class="fa fa-chevron-right pull-right" aria-hidden="true"></i></p>
     						</router-link>
     					</span>
-    					<div class="text-right  col-sm-6" v-if="promo">
+    					<div class="text-right  col-sm-4" v-if="promo">
         					<social-sharing :url="shareURL(promo.slug)" :title="promo.title" :description="promo.body" :quote="_.truncate(promo.description, {'length': 99})" twitter-user="EastgateSquare" :media="promo.image_url" inline-template >
                             <div class="blog-social-share">
                                 <div class="social_share">
