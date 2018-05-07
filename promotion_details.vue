c13bdad75a2174cd89b20b307f0692476df07427
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-07 15:14:09 -0400

    Eastgate Square 2.0 1.1  /promotion_details.vue

--- promotion_details.vue
+++ promotion_details.vue
@@ -7,7 +7,7 @@
 				</div>
 			</div>
 			<div class="col-sm-8 event_image_container text-left">
-				<img :src="currentPromo.image_url" :alt="currentPromo.store.name"/>
+				<img :src="currentPromo.image_url" :alt="currentPromo.name"/>
 			</div>
 		</div>
 		<div class="row" style="margin-left:0; margin-top: 20px;">
