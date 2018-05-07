081220483850cce408cbb8202972f595baf48858
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-07 17:18:52 -0400

    Eastgate Square 2.0 1.1  /contact_us.vue

--- contact_us.vue
+++ contact_us.vue
@@ -23,7 +23,7 @@
                         <div class="form-group">
                             <div class="col-sm-6 col-xs-12" :class="{'has-error': errors.has('phone')}">
                                 <label class="label" for="phone">Phone</label>
-                                <input v-model="form_data.phone" v-validate="'required|alpha_dash'" class="form-control" :class="{'input': true}" name="phone" type="phone" placeholder="Phone" data-vv-delay="500">
+                                <input v-model="form_data.phone" v-validate="'required|alpha_dash'" class="form-control" :class="{'input': true}" name="phone" type="phone" placeholder="Phone" data-vv-delay="500" id=form_phone">
                                 <span v-show="errors.has('phone')" class="form-control-feedback">{{ errors.first('phone') }}</span>
                             </div>
                             <div class="col-sm-6 col-xs-12" :class="{'has-error': errors.has('subject')}">
