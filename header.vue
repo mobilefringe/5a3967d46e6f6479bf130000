bed346bd67bc74af49d7c3cba27fef6ecbe19edb
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-09 16:12:29 -0400

    Eastgate Square 2.0 1.1  /header.vue

--- header.vue
+++ header.vue
@@ -88,7 +88,10 @@
     					<a href="#" class="menu_link dropdown-toggle" :id="item.name.id" @click="toggleSubMenu(item.id); item.show_sub_menu = !item.show_sub_menu" data-toggle="dropdown" aria-haspopup="true" :aria-expanded="item.show_sub_menu">{{item.name}} </a>
     					<transition name="custom-classes-transition" enter-active-class="animated fadeIn" leave-active-class="animated fadeOut">
     						<div class="dropdown-menu" v-if="item.sub_menu" v-show="item.show_sub_menu">
-    							<router-link tag="a" class="dropdown-item" v-for="sub_menu in item.sub_menu" :to="sub_menu.href">{{sub_menu.name}}</router-link>
+    						    <li>
+    						        <router-link tag="a" class="dropdown-item" v-for="sub_menu in item.sub_menu" :to="sub_menu.href">{{sub_menu.name}}</router-link>
+    						    </li>
+    							
     						</div>
     					</transition>
     				</div>
