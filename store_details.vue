ad3ebdbc3b50ebfaf2cc827eb90aea17eceadd94
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-11 17:00:39 -0400

    Eastgate Square 2.0 1.1  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -199,6 +199,18 @@
                 pngMapRef() {
                     return this.$refs.pngmapref;
                 },
+                floorList () {
+                    var floor_list = [];
+                    
+                    var floor_1 = {};
+                    floor_1.id = "first-floor";
+                    floor_1.title = "Level One";
+                    floor_1.map = this.getSVGurl;
+                    floor_1.z_index = 1;
+                    floor_1.show = true;
+                    floor_list.push(floor_1);
+                    return floor_list;
+                }
             },
             methods: {
                 loadData: async function() {
