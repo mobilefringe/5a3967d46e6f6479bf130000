d35acd7666efd3b3a057fa1775fe9e289c833665
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-29 13:45:36 -0400

    Eastgate Square 2.0 1.1  /community.vue

--- community.vue
+++ community.vue
@@ -170,6 +170,75 @@
         top: initial;
         bottom: -27px;
     }
+    #community_container .modal-mask {
+        position: fixed;
+        z-index: 9998;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, .5);
+        display: table;
+        transition: opacity .3s ease;
+    }
+
+    #community_container .modal-wrapper {
+        display: table-cell;
+        vertical-align: middle;
+    }
+    
+    #community_container .modal-container {
+        width: 300px;
+        margin: 0px auto;
+        padding: 20px 30px;
+        background-color: #fff;
+        border-radius: 2px;
+        box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
+        transition: all .3s ease;
+        font-family: Helvetica, Arial, sans-serif;
+    }
+    #community_container .modal-header {
+        border-bottom: none;
+    }
+    #community_container .modal-header h2 {
+        margin: 0 auto;
+        color: #ed2227;
+        border-bottom: none;
+        font-family: Helvetica,Arial,sans-serif;
+        font-weight: bold;
+    }
+    
+    #community_container .modal-footer {
+      /*margin: 20px 0;*/
+      border-top: none;
+    }
+    
+    #community_container .modal-default-button {
+        float: right;
+    }
+    
+    /*
+     * The following styles are auto-applied to elements with
+     * transition="modal" when their visibility is toggled
+     * by Vue.js.
+     *
+     * You can easily play with the modal transition by editing
+     * these styles.
+     */
+    
+    #community_container .modal-enter {
+        opacity: 0;
+    }
+    
+    #community_container .modal-leave-active {
+        opacity: 0;
+    }
+    
+    #community_container .modal-enter .modal-container,
+    #community_container .modal-leave-active .modal-container {
+        -webkit-transform: scale(1.1);
+        transform: scale(1.1);
+    }
 </style>
 
 <script>
