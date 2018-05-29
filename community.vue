27cbc164ee678204d37e1e67e10f97676fe658da
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-29 11:43:35 -0400

    Eastgate Square 2.0 1.1  /community.vue

--- community.vue
+++ community.vue
@@ -234,7 +234,7 @@
                             //format email
                             send_data = {};
                             send_data.url = "http://www.mallmaverick.com/send_contact_email";
-                            var formatted_formdata = {}; //JSON.stringify(this.serializeObject(this.form_data));
+                            var formatted_formdata = {}; 
                             formatted_formdata.send_to = "BMiele@Bentallkennedy.com,kkelly@bentallkennedy.com,swhitaker@bentallkennedy.com";
                             formatted_formdata.subject = "Eastgate Community Rental Form Application"; 
                             formatted_formdata.body = {};
