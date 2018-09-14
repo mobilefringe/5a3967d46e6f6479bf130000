dac7e0481273ce0720ffcf27c72644d911a755be
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-09-14 10:23:28 -0400

    Eastgate Square 2.0 1.1  /jobs.vue

--- jobs.vue
+++ jobs.vue
@@ -114,7 +114,7 @@
                         }
                     });
                     _.sortBy(this.processedJobs, [function(o) {
-                        return o.show_on_web_date;
+                        return o.end_date;
                     }]);
                     return this.processedJobs;
                 }
