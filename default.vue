882b1ec5afb0ede3fb86ecf903a109aaa6398645
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2020-05-14 12:24:26 -0400

    Eastgate Square 2.0 1.1  /default.vue

--- default.vue
+++ default.vue
@@ -6,6 +6,24 @@
   define(["Vue", "vue-meta"], function(Vue, Meta) {
     return Vue.component("default-component", {
       template: template, // the variable template will be injected
+      head: {
+            // To use "this" in the component, it is necessary to return the object through a function
+            title: function () {
+              return {
+                inner: this.meta.meta_title,
+                separator: ' ', // Leave empty separator
+                complement: ' ' // Leave empty complement
+              }
+            },
+            meta: function () {
+              return [
+                 { name: 'description', id: 'description', content: this.meta.meta_description },
+                 { name: 'keywords',  id: 'keywords', content: this.meta.meta_keywords },
+                 { property: 'og:title', id: 'og:title', content: this.meta.meta_title },
+                 { property: 'og:description', id: 'og:description', content: this.meta.meta_description }
+              ]
+            }
+        },
       data: function() {
         return {
             meta: {
@@ -20,11 +38,13 @@
         next(vm => {
           // access to component instance via `vm`
           vm.meta = vm.findMetaDataByPath(to.path);
-          console.log(vm.meta);
+          vm.$emit('updateHead')
+          console.log('vm.meta', vm.meta, 'to.path', to.path)
         })
       },
       beforeRouteUpdate (to, from, next) {
         this.meta = this.findMetaDataByPath(to.path);
+        this.$emit('updateHead')
         next();
       },
       computed: {
