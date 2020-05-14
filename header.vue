e7492865815eae9b72c98e57ff5e1e8fa9bb9ffe
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2020-05-14 11:45:57 -0400

    Eastgate Square 2.0 1.1  /header.vue

--- header.vue
+++ header.vue
@@ -9,9 +9,9 @@
         					<img class="site-logo" src="//codecloud.cdn.speedyrails.net/sites/5af082f76e6f6450e9040000/image/png/1516652184126/ES_logo_red.png" alt="Eastgate Square">
         				</router-link>
         				<div class="header-ribbon">
-        				    <a target="_blank" href="https://kingsleyassociates.com/kingsley-excellence" rel="noopener noreferrer" aria-label="Visit Kingley Associates website">
-                                <img height="100px" width="100px" src="http://docs.kingsleyportal.com/Images/Awards/Commercial2019Emblem.svg" alt="2019 Kingsley Excellence Tenant Satisfaction Emblem" alt="2019 Kingsley Excellence Tenant Satisfaction Emblem"/>
-                            </a>
+        				    <!--<a target="_blank" href="https://kingsleyassociates.com/kingsley-excellence" rel="noopener noreferrer" aria-label="Visit Kingley Associates website">-->
+                <!--                <img height="100px" width="100px" src="http://docs.kingsleyportal.com/Images/Awards/Commercial2019Emblem.svg" alt="2019 Kingsley Excellence Tenant Satisfaction Emblem" alt="2019 Kingsley Excellence Tenant Satisfaction Emblem" />-->
+                <!--            </a>-->
         				</div>
     			    </div>
     				
